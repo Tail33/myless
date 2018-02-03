77e3b4c75496e24e535dcc95c837466488f9f203
Tail33 <295063867@qq.com>
2018-02-04 00:58:00 +0800

    下拉框；三角形

--- myless.js
+++ myless.js
@@ -1,3 +1,4 @@
+
 /**
  * 1.模拟下拉框
  */
@@ -12,27 +13,35 @@ var Select = function (elAry) {
 Select.prototype.init = function () {
 
     var self = this;
-    Array.prototype.forEach.call(self.elAry, function (item) { //遍历对象集合 
+    Array.prototype.forEach.call(self.elAry, function (item,index,array) { //遍历对象集合 
         var select = item.querySelector(".select");
         var subBox = item.querySelector(".subBox");
 
         //实现点击下拉效果，再次点击隐藏，点击其他地方也会隐藏下拉列表。
         select.onclick = function (e) {
+            Array.prototype.forEach.call(array,function(item){
+                item.querySelector(".subBox").style.display="none";
+            });
             var computedStyle = document.defaultView.getComputedStyle(subBox, null);
             if (computedStyle.display === "none") {
                 subBox.style.display = "block";
+                
             } else {
                 subBox.style.display = "none";
             }
             e.stopPropagation(); //阻止冒泡
+            
         };
-        document.onclick = function () {
+        document.addEventListener('click',function () {
             subBox.style.display = "none";
-        }
+        });
 
         //绑定下拉列表hover的效果
         var list = subBox.querySelectorAll("li");
         Array.prototype.forEach.call(list, function (item) {
+            item.onclick=function(){
+                select.querySelector("span").innerHTML=item.innerHTML;
+            };
             item.onmouseover = function () {
                 item.style.backgroundColor = "red";
             };
